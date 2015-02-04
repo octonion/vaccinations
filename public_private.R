c0abda89e8c03272874059f115c48312939353a7
Christopher D. Long <galizur@gmail.com>
2015-02-04 18:24:31 -0500

    Simple vaccination rate model.

--- public_private.R
+++ public_private.R
@@ -1,5 +1,16 @@
 
+library(lme4)
+
 rates <- read.csv("data/cakd1314.csv", header=TRUE)
 
 head(rates)
 summary(rates)
+colnames(rates)
+
+fit <- glmer(cbind(PBE.,ENROLLMENT-PBE.) ~ (1|COUNTY) + (1|CITY) + PUBLIC.PRIVATE, data=rates, family="binomial")
+
+fit
+summary(fit)
+
+fixef(fit)
+ranef(fit)
